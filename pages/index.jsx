4c7aa78014a61681b02d03bde654413cf7c2235a
Apollos Iwuamadi <apollosiwuamadi@gmail.com>
2023-09-15 15:16:49 +0100

    first commit

---
+++ pages/index.jsx
@@ -0,0 +1,22 @@
+import Tools from "./tools"
+
+export default function Home(){
+
+
+}
+return(
+    <Tools myName="Apollos" skill1="Html" skill2="CSS" skill3="Bootsrap"/>
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ pages/tools.jsx
@@ -0,0 +1,21 @@
+export default function Tools(props){
+    return(
+        <>
+         <p> my name is {props.myName}</p>
+         <p> my skillset includes:</p>
+         <ul>
+            <li>{props.skill1}</li>
+         </ul>
+        </>
+
+    )
+}
+
+
+
+
+
+
+
+
+
